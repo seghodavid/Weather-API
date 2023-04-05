65b5b4efd513b75eace0fd4852a3a6b324f3c281
seghodavid <56871763+seghodavid@users.noreply.github.com>
2023-04-05 12:22:32 +0100

    app.js setup

--- app.js
+++ app.js
@@ -1,41 +1,52 @@
+require('dotenv').config()
 const express = require("express")
 const path = require('path')
 const axios = require('axios').default
 
 
+
 const app = express()
 
 app.use(express.json())
-app.set
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs')
+app.use(express.static(path.join(__dirname, 'public')))
 
-axios
-  .get("https://api.openweathermap.org/data/2.5/weather", {
-    params: {
-      lat: 14.6147396,
-      lon: 0.9445775,
-      appid: "e9644f71bb295f373795aecac30f165e",
-    },
-  })
-  .then((response) => {
-    console.log(response.data.weather);
-    // return response.weather
-  })
-  .catch((error) => {
-    console.log(error);
-  });
-axios
-  .get("http://api.openweathermap.org/geo/1.0/direct", {
-    params: {
-      q: "Abuja",
-      limit: 2,
-      appid: "e9644f71bb295f373795aecac30f165e",
-    },
-  })
-  .then((response) => {
-    console.log(response.data);
-    // return response.weather
-  })
-  .catch((error) => {
-    console.log(error);
-  });
 
+
+// axios
+//   .get("https://api.openweathermap.org/data/2.5/weather", {
+//     params: {
+//       lat: 14.6147396,
+//       lon: 0.9445775,
+//       appid: "",
+//     },
+//   })
+//   .then((response) => {
+//     console.log(response.data.weather);
+//     // return response.weather
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+// axios
+//   .get("http://api.openweathermap.org/geo/1.0/direct", {
+//     params: {
+//       q: "Abuja",
+//       limit: 2,
+//       appid: "",
+//     },
+//   })
+//   .then((response) => {
+//     console.log(response.data);
+//     // return response.weather
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+const PORT = 8080 || process.env.PORT
+const start = () => {
+    app.listen(PORT, ()=> console.log(`Server is running...`))
+}
+
+start()
